976e3824f1972672211589c1836dca6dd02ee205
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2022-03-13 18:41:39 -0300

    exemplos

---
+++ bin/exemplos/instrucoes_de_controle.dart
@@ -0,0 +1,24 @@
+void instrucoesDeControle() {
+  // if e else
+
+  const num = 2;
+
+  if (num == 0) {
+    print('num == 0');
+  } else if (num == 1) {
+    print('num == 1');
+  } else {
+    print('num == 2');
+  }
+
+  // for loops
+
+  for (var i = 0; i < num; i++) {
+    print(i);
+  }
+
+  // while and do-while loops
+  // break and continue
+  // switch and case
+  // assert
+}
